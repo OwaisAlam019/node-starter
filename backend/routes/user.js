46f2947c2605720c86dc9b480fbf46b89b5342d5
Owais Alam <owaisstar@gmail.com>
2018-11-11 21:10:34 +0500

    login route creation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -32,43 +32,14 @@ router.post("/signup", (req,res,next) => {
   
 })
 
-// router.post("/signup", (req, res, next) => {
-//   res.send("Hello signup",req.body)
-  
-// });
-
-// router.post("/login", (req, res, next) => {
-//   let fetchedUser;
-//   User.findOne({ email: req.body.email })
-//     .then(user => {
-//       if (!user) {
-//         return res.status(401).json({
-//           message: "Auth failed"
-//         });
-//       }
-//       fetchedUser = user;
-//       return bcrypt.compare(req.body.password, user.password);
-//     })
-//     .then(result => {
-//       if (!result) {
-//         return res.status(401).json({
-//           message: "Auth failed"
-//         });
-//       }
-//       const token = jwt.sign(
-//         { email: fetchedUser.email, userId: fetchedUser._id },
-//         "secret_this_should_be_longer",
-//         { expiresIn: "1h" }
-//       );
-//       res.status(200).json({
-//         token: token
-//       });
-//     })
-//     .catch(err => {
-//       return res.status(401).json({
-//         message: "Auth failed"
-//       });
-//     });
-// });
+router.post("/login",(req,res,next)=>{
+  User.findOne({email:req.body.email},(user) => {
+    if(!user){
+      res.status(404).json({
+        message:"User does not exist"
+      })
+    }
+  });
+});
 
 module.exports = router;
