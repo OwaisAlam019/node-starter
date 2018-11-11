f26019b3ab7bafe58dc2e1e060cc2a0c504bd880
Owais Alam <owaisstar@gmail.com>
2018-11-11 18:45:44 +0500

    user signup with mlab

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb://root:admin1234@ds229295.mlab.com:29295/todo"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/user", userRoutes);
+
+module.exports = app;
