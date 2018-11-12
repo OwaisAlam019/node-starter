b7ef52002b290a1f6c859a865f2793d1c1ff37f3
Owais Alam <owaisstar@gmail.com>
2018-11-12 10:23:00 +0500

    returning token on successful user login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 var bodyParser = require('body-parser')
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const router = express.Router();
 
@@ -33,13 +33,31 @@ router.post("/signup", (req,res,next) => {
 })
 
 router.post("/login",(req,res,next)=>{
-  User.findOne({email:req.body.email},(user) => {
-    if(!user){
+  let loggedinUser;
+  User.findOne({email:req.body.email},(err,user) => {
+    loggedinUser = user;
+    // console.log(user,req.body)
+    if(!loggedinUser){
       res.status(404).json({
         message:"User does not exist"
       })
     }
-  });
+    return bcrypt.compare(req.body.password,loggedinUser.password)
+  }).then(result => {
+    console.log("result",result)
+    if(result){
+      console.log("got result")
+      const token = jwt.sign({email:loggedinUser.email,userId:loggedinUser._id},
+        "long_secret_key",
+        {expiresIn:"1h"});
+        res.status(200).json(
+          { token:token }
+        )
+    }
+  }).catch(err => {
+    console.log("catch error",err)
+   return res.status(401).json({Error:err})
+  })
 });
 
 module.exports = router;
